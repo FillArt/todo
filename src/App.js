2d3532a7e48fb2cf4679212d336dce78a08506ee
FillArt <frontend.fillart@gmail.com>
2021-03-20 19:52:00 +0200

    Added app markup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+	    <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 ">
+        <div className="mb-4">
+            <h1 className="text-grey-darkest">Cписок задач на React</h1>
+            <div class="flex mt-4">
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-500" placeholder="Добавить задачу" />
+                <button className="flex-no-shrink p-2 border-2 rounded text-blue-500 border-blue-500 hover:text-white hover:bg-blue-500">Добавить</button>
+            </div>
+        </div>
+        <div>
+            <div className="flex mb-4 items-center">
+                <p className="w-full text-grey-darkest">Первая задача</p>
+                <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-500 border-green-500 hover:bg-green-500">Готово</button>
+                <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500">Удалить</button>
+            </div>
+          	<div className="flex mb-4 items-center">
+                <p className="w-full line-through text-green">Вторая задача</p>
+                <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-gray-500 border-gray-500 hover:bg-gray-500">Не готово</button>
+                <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500">Удалить</button>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
