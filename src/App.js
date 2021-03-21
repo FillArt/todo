a959cdfec5fcc82d64c59418aec808e492825b04
FillArt <frontend.fillart@gmail.com>
2021-03-21 10:31:17 +0200

    Minor refactoring

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
-import { TodoInput } from "./components/TodoInput";
-import { TodoList } from "./components/TodoList";
+import React, { useState } from 'react'
+import { TodoInput } from './components/TodoInput'
+import { TodoItem } from './components/TodoItem'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      task: 'Тестовая задача',
+    },
+  ])
 
-  const [todos, setTodos] = useState([{
-    id: 1,
-    task: 'Тестовая задача'
-  }]);
-
-  const addTodoList = (name) => {
-    setTodos((oldList) => [...oldList, {
-      id: Date.now,
-      task: name
-    }])
+  const addTodoList = name => {
+    setTodos(oldList => [
+      ...oldList,
+      {
+        id: Date.now,
+        task: name,
+      },
+    ])
   }
 
   return (
     <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
-	    <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 ">
+      <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 ">
         <div className="mb-4">
-            <h1 className="text-grey-darkest">Cписок задач на React</h1>
-            <TodoInput add={addTodoList} />
+          <h1 className="text-grey-darkest">Cписок задач на React</h1>
+          <TodoInput add={addTodoList} />
         </div>
         <div>
-          <TodoList todos={todos} />
+          {todos.map(item => (
+            <TodoItem key={item.id} todo={item} />
+          ))}
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
