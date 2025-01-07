91eefe09570bada7ed109e0762ef51a6e196eb99
FillArt <artiom.filipopovschii@gmail.com>
2025-01-07 17:08:10 -0400

    feat: added basic universal button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
-const Button = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+type BaseButtonProps = {
+    title: string;
+    onClick: () => void;
+    disabled?: boolean;
+}
+
+export const BaseButton = (props: BaseButtonProps) => {
+    const { title, onClick, disabled = false} = props;
+    const onClickHandler = () => onClick();
+
+    return <Button disabled={disabled} onClick={onClickHandler} variant="contained">{title}</Button>;
 };
-
-export default Button;
