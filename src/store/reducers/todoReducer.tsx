55c93c585560d315e871e0cf00c9ccca1380d71a
FillArt <artiom.filipopovschii@gmail.com>
2025-01-28 20:05:29 -0400

    refactor: minor fix create actions function.

--- src/store/reducers/todoReducer.tsx
+++ src/store/reducers/todoReducer.tsx
@@ -32,46 +32,26 @@ export const todoReducer = (state: State[] = initialState, action: TsarType): St
     }
 }
 
-export const addTodo = (title: string) => {
-    return {
-        type: 'ADD-TODO',
-        payload: {
-            title
-        }
-    } as const
-}
+export const addTodo = (title: string) => ({
+    type: 'ADD-TODO',
+    payload: {title}
+} as const)
 
-export const removeTodo = (id: string) => {
-    return {
-        type: 'REMOVE-TODO',
-        payload: {
-            id
-        }
-    } as const
-}
+export const removeTodo = (id: string) => ({
+    type: 'REMOVE-TODO',
+    payload: {id}
+} as const)
 
-export const changeTodo = (id: string, title: string) => {
-    return {
-        type: 'CHANGE-TODO',
-        payload: {
-            id,
-            title
-        }
-    } as const
-}
+export const changeTodo = (id: string, title: string) => ({
+    type: 'CHANGE-TODO',
+    payload: {id, title}
+} as const)
 
-export const changeStatusTodo = (id: string, isDone: boolean)=> {
-    return {
-        type: 'CHANGE-STATUS',
-        payload: {
-            id,
-            isDone
-        }
-    } as const
-}
+export const changeStatusTodo = (id: string, isDone: boolean) => ({
+    type: 'CHANGE-STATUS',
+    payload: {id, isDone}
+} as const)
 
-export const removeAllTodo = ()=> {
-    return {
-        type: 'REMOVE-ALL'
-    } as const
-}
+export const removeAllTodo = () => ({
+    type: 'REMOVE-ALL'
+} as const)
